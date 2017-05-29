6a6c227be10c8146afec16a59a8f8c4d11474cd9
Ic3Venom <Ic3Venom@gmail.com>
2017-05-28 23:10:17 -0700

    Update grapher.py

--- grapher.py
+++ grapher.py
@@ -1,3 +1,4 @@
+from operator import index
 class Term:
     
     def __init__(self, xvalue):
@@ -6,10 +7,13 @@ class Term:
 
 
 def main():
-    pass
+    resolution    = [0,0,raw_input("What resolution do you want your graph to be? (Format: x*y): ")]
+    resolution[0] = resolution[2][:resolution[2].index('*')]
+    resolution[1] = resolution[2][resolution[2].index('*') +1:]
+    
+    print resolution
  
 if __name__ == '__main__':
     main()
     
     exit
-    
